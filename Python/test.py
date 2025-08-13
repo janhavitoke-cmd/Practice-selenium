7a47f06df08f1764cff09a67067a967af8dc3ec8
Janhavi Toke <janhavi.toke@gammaedge.io>
2025-08-13 14:47:33 +0530

    test updated

--- Python/test.py
+++ Python/test.py
@@ -1,22 +1,52 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
-driver = webdriver.Chrome(service= Service(ChromeDriverManager().install()))
-driver.get("https://www.facebook.com/")
+driver = webdriver.Chrome() #initialized webdriver
+
+driver.get("https://rahulshettyacademy.com/client/#/auth/register")
 driver.maximize_window()
 
-time.sleep(2)
+driver.find_element(By.XPATH, "//input[@id='firstName']").send_keys("Robb")
+driver.find_element(By.XPATH,"//input[@id='lastName']").send_keys("banner")
+driver.find_element(By.XPATH,"//input[@id='userEmail']").send_keys("robbinbanner@gmail.com")
+driver.find_element(By.XPATH,"//input[@id='userMobile']").send_keys("9898989898")
 
-driver.find_element(By.ID,"email").send_keys("janhavi")
-driver.find_element(By.ID,"pass").send_keys("122121")
+occupation_dropdown = Select(driver.find_element(By.XPATH, "//select[contains(@class,'custom-select')]"))
+occupation_dropdown.select_by_visible_text("Engineer")
 
-driver.find_element(By.NAME,"Login").click()
+driver.find_element(By.XPATH, "//input[@type='radio' and @value='Female']").click()
 
-"""
-hyfrtdrtd
-"""
-time.sleep(6)
+driver.find_element(By.XPATH,"//input[@id='userPassword']").send_keys("#123@Jan")
+driver.find_element(By.XPATH,"//input[@id='confirmPassword']").send_keys("#123@Jan")
+
+driver.find_element(By.XPATH,"//input[@type='checkbox']").click()
+
+regbutton = WebDriverWait(driver,200).until(EC.element_to_be_clickable((By.XPATH,"//input[@id='login']")))
+regbutton.click()
+
+wait = WebDriverWait(driver,300).until(EC.element_to_be_clickable((By.ID,"login")))
+
+login = WebDriverWait(driver, 400).until(
+    EC.element_to_be_clickable((By.XPATH, "//a[text()='Login here']"))
+)
+login.click()
+time.sleep(10)
+
+'''''
+driver.get("https://rahulshettyacademy.com/client/#/auth/login")
+driver.maximize_window()
+
+driver.find_element(By.ID,"userName").send_keys("Robb")
+driver.find_element(By.ID,"userPassword").send_keys("banner")
+WebDriverWait(driver,300).until(EC.element_to_be_clickable((By.ID,"login")))
+time.sleep(10)
+'''
+
 driver.quit()
+
+
+
